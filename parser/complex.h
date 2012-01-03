8954e58cb1b7dab3d9b371f3a216162909a88419
Jim Holmström <jim.holmstroem@gmail.com>
2012-01-03 18:48:01 +0100

    Almost forgot namespace

--- parser/complex.h
+++ parser/complex.h
@@ -3,61 +3,64 @@
 
 #include <cmath>
 
-struct complex
-{
-    double real;
-    double imag;
+namespace parser {
 
-    complex(double r,double i)
-        : real(r)
-        , imag(i)
+    struct complex
     {
-    };
-    complex(double r) //TODO write something about automatic conversion and non-excplicit constructor in c++
-        : real(r)
-        , imag(0)
-    {
-    };
+        double real;
+        double imag;
 
-    complex operator+(complex c)
-    {
-        return complex(real+c.real,imag+c.imag); 
-    };
-    complex operator-(complex c)
-    {
-        return complex(real-c.real,imag-c.imag);
-    };
-    complex operator*(complex c)
-    {
-        return complex( real*c.real-imag*c.imag , real*c.imag+c.imag*c.real );
-    };
-    complex operator*(double c) //TODO write something about these being only shortcuts, not really needed since the non-excplicit constructur deals with it converting it to an complex as needed.
-    {
-        return complex(c*real,c*imag);
-    };
-    complex conj()
-    {
-        return complex(real,-imag);
-    };
-    double abs()
-    {
-        return sqrt(this->abs_square());
-    };
-    double abs_square()
-    {
-        return real*real+imag*imag;
-    };
-    complex operator/(double d) 
-    {
-        return complex(real/d,imag/d);
-    };
-    complex operator/(complex c)
-    {
-        return ((*this)*c.conj())/c.abs_square();
-    };
+        complex(double r,double i)
+            : real(r)
+            , imag(i)
+        {
+        };
+        complex(double r) //TODO write something about automatic conversion and non-excplicit constructor in c++
+            : real(r)
+            , imag(0)
+        {
+        };
 
-//TODO should this be supported, why?    complex double() 
+        complex operator+(complex c)
+        {
+            return complex(real+c.real,imag+c.imag); 
+        };
+        complex operator-(complex c)
+        {
+            return complex(real-c.real,imag-c.imag);
+        };
+        complex operator*(complex c)
+        {
+            return complex( real*c.real-imag*c.imag , real*c.imag+c.imag*c.real );
+        };
+        complex operator*(double c) //TODO write something about these being only shortcuts, not really needed since the non-excplicit constructur deals with it converting it to an complex as needed.
+        {
+            return complex(c*real,c*imag);
+        };
+        complex conj()
+        {
+            return complex(real,-imag);
+        };
+        double abs()
+        {
+            return sqrt(this->abs_square());
+        };
+        double abs_square()
+        {
+            return real*real+imag*imag;
+        };
+        complex operator/(double d) 
+        {
+            return complex(real/d,imag/d);
+        };
+        complex operator/(complex c)
+        {
+            return ((*this)*c.conj())/c.abs_square();
+        };
+
+    //TODO should this be supported, why?    complex double() 
+
+    };
 
 };
-
 #endif
