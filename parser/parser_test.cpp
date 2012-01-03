4e4575ef407b7b244e163cbff74a2f83e4bfac87
Jim Holmström <jim.holmstroem@gmail.com>
2012-01-03 18:50:20 +0100

    Indendation miss

--- parser/parser_test.cpp
+++ parser/parser_test.cpp
@@ -12,15 +12,15 @@ main(int argc,const char* argv[]) {
     parser::parser p = parser::parser();
     std::cout.precision(15);
 	
-	parser::iexpression* e = p.parse(argv[1]);
+    parser::iexpression* e = p.parse(argv[1]);
 
-	std::stringstream ss;
-	ss.str(argv[2]);
-	double x;
-	ss>>x;
+    std::stringstream ss;
+    ss.str(argv[2]);
+    double x;
+    ss>>x;
 
-	std::cout << e->eval(x) << std::endl;
+    std::cout << e->eval(x) << std::endl;
 
-        delete e;
+    delete e;
 
 };
